933fec99b63fe062776f5c033662a9f802b9fdea
Koustubh777 <vatsakartikeya41@gmail.com>
2025-12-11 15:27:23 +0530

    Update Game.jsx

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import HUD from '../components/HUD'
 import { createScene } from '../game/babylonScene'
 
-
 export default function Game({ user, room, onExit }){
-const canvasRef = React.useRef()
-React.useEffect(()=>{
-const engineScene = createScene(canvasRef.current, user, room)
-return ()=> engineScene.dispose && engineScene.dispose()
-},[])
+  const canvasRef = useRef()
+  const [sceneObj, setSceneObj] = useState(null)
 
+  useEffect(()=>{
+    const engineScene = createScene(canvasRef.current, user, room)
+    setSceneObj(engineScene)
+    return ()=> engineScene.dispose && engineScene.dispose()
+  },[])
 
-return (
-<div className="game-root">
-<canvas ref={canvasRef} id="renderCanvas" />
-<HUD onExit={onExit} />
-</div>
-)
+  const handleMove = ({x, z})=>{
+    if(sceneObj) sceneObj.updateLocalMovement(x, z)
+  }
+
+  const handleAbility = (ability)=>{
+    if(sceneObj) sceneObj.useAbility(ability)
+  }
+
+  return (
+    <div className="game-root">
+      <canvas ref={canvasRef} id="renderCanvas" />
+      <HUD onExit={onExit} onMove={handleMove} onAbility={handleAbility} />
+    </div>
+  )
 }
