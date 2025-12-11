c58cbf66c7553b678c44aaca28e07505b5ba6c1e
Koustubh777 <vatsakartikeya41@gmail.com>
2025-12-11 14:20:40 +0530

    Update Game.jsx

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,1 +1,20 @@
+import React from 'react'
+import HUD from '../components/HUD'
+import { createScene } from '../game/babylonScene'
 
+
+export default function Game({ user, room, onExit }){
+const canvasRef = React.useRef()
+React.useEffect(()=>{
+const engineScene = createScene(canvasRef.current, user, room)
+return ()=> engineScene.dispose && engineScene.dispose()
+},[])
+
+
+return (
+<div className="game-root">
+<canvas ref={canvasRef} id="renderCanvas" />
+<HUD onExit={onExit} />
+</div>
+)
+}
