5de1e72e03928bf090aae319db01289c797e9b69
Koustubh777 <vatsakartikeya41@gmail.com>
2025-12-11 14:06:12 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,1 +1,9 @@
-
+{
+apiKey: "REPLACE_API_KEY",
+authDomain: "REPLACE_AUTH_DOMAIN",
+projectId: "REPLACE_PROJECT_ID",
+storageBucket: "REPLACE_STORAGE_BUCKET",
+messagingSenderId: "REPLACE_MSG_SENDER_ID",
+appId: "REPLACE_APP_ID",
+databaseURL: "REPLACE_DB_URL" // for Realtime DB
+}
