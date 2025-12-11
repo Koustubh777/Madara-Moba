e88a12cb45fdedafe9f236789fc3cb807fb0b622
Koustubh777 <vatsakartikeya41@gmail.com>
2025-12-11 16:33:29 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,21 @@
-export const firebaseConfig = {
-apiKey: "AIzaSyBYh0X-ClFEmNs3o2uXjEgyWYSq4Q8R-4Y",
-authDomain: "madara-moba-e9ece.firebaseapp.com",
-projectId: "madara-moba-e9ece",
-storageBucket: "madara-moba-e9ece.firebasestorage.app",
-messagingSenderId: "722972485578",
-appId: "1:722972485578:web:d28c952cc040c78b2dd133" ,
-  databaseURL: "https://madara-moba-e9ece-default-rtdb.firebaseio.com/"
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYh0X-ClFEmNs3o2uXjEgyWYSq4Q8R-4Y",
+  authDomain: "madara-moba-e9ece.firebaseapp.com",
+  databaseURL: "https://madara-moba-e9ece-default-rtdb.firebaseio.com",
+  projectId: "madara-moba-e9ece",
+  storageBucket: "madara-moba-e9ece.firebasestorage.app",
+  messagingSenderId: "185739151066",
+  appId: "1:185739151066:web:62cfe21dce7bace7320f7f"
 };
 
-export default firebaseConfig
-export { rtdb };
+// ✅ Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// ✅ Initialize Realtime Database
+const rtdb = getDatabase(app);
+
+// ❗ Export BOTH
+export { firebaseConfig, app, rtdb };
